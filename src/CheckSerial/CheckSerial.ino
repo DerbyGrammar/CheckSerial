7d6563a6d6fdb3055f39bbe8c8f1f9e6dc2a6a9f
Thorin <c.nethercott@icloud.com>
2016-04-15 14:09:58 +0100

    Update CheckSerial.ino

--- src/CheckSerial/CheckSerial.ino
+++ src/CheckSerial/CheckSerial.ino
@@ -1,1 +1,51 @@
+/*
+  ThorinDev / Chris Nethercott
+  Check Serial
+*/
 
+const int tx = 1;
+const int rx = 0;
+char inSerial[15];
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(tx, OUTPUT);
+  pinMode(rx, INPUT);
+}
+
+void loop() {
+  int i = 0;
+  int m = 0;
+  
+  delay(500);
+
+  if(Serial.available() > 0) {
+    while(Serial.available() > 0) {
+      inSerial[i] = Serial.read();
+      i++;
+    }
+    inSerial[i] = '\0';
+    CheckProtocol(inSerial);
+  }
+}
+
+void CheckProtocol(char inStr[]) {
+  int i = 0;
+  int m = 0;
+
+  Serial.println(inStr);
+
+  if(!strcmp(inStr,"helloworld")) {
+    // What do you want to happen?
+    helloworld();
+    for(m = 0; m < 11; m++) {
+      inStr[m] = 0;
+    }
+    i = 0;
+  }
+}
+
+void helloworld() {
+  Serial.println("Hello World!");
+}
